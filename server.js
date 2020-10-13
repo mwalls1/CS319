0b7df3850a2c83ada66fbe8fc815753010a399ce
mwalls1 <walls594@gmail.com>
2020-10-13 12:28:25 -0500

    Week 1 Stuff

---
+++ server.js
@@ -0,0 +1,89 @@
+var numPlayers = 0;
+var pTurn = 1;
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+app.set('port', process.env.PORT || 5000);
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+app.use(express.static(__dirname));
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+// Starts the server.
+
+// Add the WebSocket handlers
+io.on('connection', function(socket) {
+});
+
+var players = {};
+io.on('connection', function(socket) {
+  socket.on('new player', function(data) {
+	numPlayers++;
+	var temp;
+	if(numPlayers==pTurn)
+	{
+		temp = true;
+	}
+	else
+	{
+		temp = false;
+	}
+    players[socket.id] = {
+      x: 300,
+	  y: 300,
+	  num: numPlayers,
+	  name: data,
+	  isTurn: temp
+    };
+  });
+  socket.on('disconnect', function() {
+	  numPlayers--;
+	delete players[socket.id];
+  });
+  socket.on('move', function(data) {
+    var player = players[socket.id] || {};
+	if(player.isTurn)
+	{
+	if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+	if(data.next)
+	{
+		players[socket.id].isTurn = false;
+		if(pTurn >= numPlayers)
+		{
+			pTurn = 1;
+		}
+		else
+		{
+			pTurn++;
+		}
+		for (var id in players) {
+			if(players[id].num == pTurn)
+			{
+				players[id].isTurn = true;
+			}
+		}
+	}
+	}
+  });
+});
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
+server.listen(app.get('port'), function() {
+  console.log('Starting server on port 5000');
+});
---
+++ static/game.js
@@ -0,0 +1,77 @@
+var name = localStorage.getItem("name");
+var socket = io.connect();
+socket.on('message', function(data) {
+  console.log(data);
+});
+
+
+var move = {
+	up: false,
+	down: false,
+	left: false,
+	right: false,
+	next: false
+}
+document.addEventListener('keydown',function(key){
+	switch(key.keyCode){
+		case 65:
+			move.left = true;
+			break;
+		case 87:
+			move.up = true;
+			break;
+		case 68:
+			move.right = true;
+			break;
+		case 83:
+			move.down = true;
+			break;
+		case 81:
+			move.next = true;
+			break;
+	}
+});
+document.addEventListener('keyup',function(key){
+	switch(key.keyCode){
+		case 65:
+			move.left = false;
+			break;
+		case 87:
+			move.up = false;
+			break;
+		case 68:
+			move.right = false;
+			break;
+		case 83:
+			move.down = false;
+			break;
+		case 81:
+			move.next = false;
+			break;
+	}
+});
+
+socket.emit('new player', name);
+setInterval(function() {
+  socket.emit('move', move);
+}, 1000/60);
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+socket.on('state', function(players) {
+  context.clearRect(0, 0, 800, 600);
+  for (var id in players) {
+    var player = players[id];
+	context.fillStyle = 'green';
+    context.beginPath();
+    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    context.fill();
+	 context.fillStyle = 'black';
+	context.fillText(player.name, player.x-15, player.y+20);
+	if(player.isTurn)
+	{
+		context.fillText(player.name+"'s turn", 10,590);
+	}
+  }
+});
